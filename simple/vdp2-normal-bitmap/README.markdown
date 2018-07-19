1f051efaad59f80ba14f4f97d8626d5c457b13e6
Israel Jacquez <mrkotfw@gmail.com>
2018-07-18 23:19:00 -0700

    Add vdp2-normal-bitmap example

---
+++ simple/vdp2-normal-bitmap/README.markdown
@@ -0,0 +1,5 @@
+Description
+===========
+
+Purpose of this example is to make basic use of the bitmap mode for
+NBG0 and NBG1.
---
+++ simple/vdp2-normal-bitmap/vdp2-normal-bitmap.c
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2012 Israel Jacquez
+ * See LICENSE for details.
+ *
+ * Israel Jacquez <mrkotfw@gmail.com>
+ */
+
+#include <yaul.h>
+
+#include <tga.h>
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern uint8_t root_romdisk[];
+
+static void vblank_in_handler(irq_mux_handle_t *) __unused;
+
+int
+main(void)
+{
+        vdp2_init();
+
+        cpu_intc_mask_set (15); {
+                irq_mux_t *vblank_in;
+                vblank_in = vdp2_tvmd_vblank_in_irq_get();
+
+                irq_mux_handle_add(vblank_in, vblank_in_handler, NULL);
+        } cpu_intc_mask_set(0);
+
+        struct scrn_bitmap_format format;
+        memset(&format, 0x00, sizeof(format));
+
+        format.sbf_scroll_screen = SCRN_NBG0;
+        format.sbf_cc_count = SCRN_CCC_RGB_32768;
+        format.sbf_bitmap_size.width = 512;
+        format.sbf_bitmap_size.height = 256;
+        format.sbf_color_palette = 0x00000000;
+        format.sbf_bitmap_pattern = VRAM_ADDR_4MBIT(0, 0x00000);
+        format.sbf_rp_mode = 0;
+        format.sbf_sf_type = SCRN_SF_TYPE_NONE;
+        format.sbf_sf_code = SCRN_SF_CODE_A;
+        format.sbf_sf_mode = 0;
+
+        vdp2_scrn_bitmap_format_set(&format);
+        vdp2_scrn_priority_set(SCRN_NBG0, 7);
+        vdp2_scrn_display_set(SCRN_NBG0, /* no_trans = */ false);
+
+        struct vram_ctl *vram_ctl;
+        vram_ctl = vdp2_vram_control_get();
+
+        vram_ctl->vram_cycp.pt[0].t0 = VRAM_CTL_CYCP_CHPNDR_NBG0;
+        vram_ctl->vram_cycp.pt[0].t1 = VRAM_CTL_CYCP_CHPNDR_NBG0;
+        vram_ctl->vram_cycp.pt[0].t2 = VRAM_CTL_CYCP_CHPNDR_NBG0;
+        vram_ctl->vram_cycp.pt[0].t3 = VRAM_CTL_CYCP_CHPNDR_NBG0;
+        vram_ctl->vram_cycp.pt[0].t4 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[0].t5 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[0].t6 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[0].t7 = VRAM_CTL_CYCP_NO_ACCESS;
+
+        vram_ctl->vram_cycp.pt[1].t0 = VRAM_CTL_CYCP_CHPNDR_NBG0;
+        vram_ctl->vram_cycp.pt[1].t1 = VRAM_CTL_CYCP_CHPNDR_NBG0;
+        vram_ctl->vram_cycp.pt[1].t2 = VRAM_CTL_CYCP_CHPNDR_NBG0;
+        vram_ctl->vram_cycp.pt[1].t3 = VRAM_CTL_CYCP_CHPNDR_NBG0;
+        vram_ctl->vram_cycp.pt[1].t4 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[1].t5 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[1].t6 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[1].t7 = VRAM_CTL_CYCP_NO_ACCESS;
+
+        vram_ctl->vram_cycp.pt[2].t0 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[2].t1 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[2].t2 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[2].t3 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[2].t4 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[2].t5 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[2].t6 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[2].t7 = VRAM_CTL_CYCP_NO_ACCESS;
+
+        vram_ctl->vram_cycp.pt[3].t0 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[3].t1 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[3].t2 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[3].t3 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[3].t4 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[3].t5 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[3].t6 = VRAM_CTL_CYCP_NO_ACCESS;
+        vram_ctl->vram_cycp.pt[3].t7 = VRAM_CTL_CYCP_NO_ACCESS;
+
+        vdp2_vram_control_set(vram_ctl);
+
+        void *romdisk;
+        void *fh; /* File handle */
+
+        romdisk_init();
+
+        romdisk = romdisk_mount("/", root_romdisk);
+        assert(romdisk != NULL);
+
+        fh = romdisk_open(romdisk, "/BITMAP.TGA");
+        assert(fh != NULL);
+
+        uint8_t *tga_file;
+        tga_t tga;
+        int ret;
+        size_t len;
+
+        tga_file = (uint8_t *)0x20200000;
+        assert(tga_file != NULL);
+
+        len = romdisk_read(fh, tga_file, romdisk_total(fh));
+        assert(len == romdisk_total(fh));
+
+        ret = tga_read(&tga, tga_file);
+        assert(ret == TGA_FILE_OK);
+
+        tga_image_decode(&tga, (void *)VRAM_ADDR_4MBIT(0, 0x00000));
+
+        color_rgb555_t bs_color;
+        bs_color = COLOR_RGB555(0x05, 0x05, 0x07);
+
+        vdp2_scrn_back_screen_color_set(VRAM_ADDR_4MBIT(3, 0x01FFFE), bs_color);
+
+        vdp2_sprite_type_set(0);
+        vdp2_sprite_priority_set(0, 0);
+
+        vdp2_tvmd_display_res_set(TVMD_INTERLACE_NONE, TVMD_HORZ_NORMAL_A, TVMD_VERT_240);
+        vdp2_tvmd_display_set();
+
+        while (true) {
+                vdp2_tvmd_vblank_out_wait();
+                vdp2_tvmd_vblank_in_wait();
+        }
+
+        return 0;
+}
+
+static void
+vblank_in_handler(irq_mux_handle_t *irq_mux __unused)
+{
+        vdp2_commit();
+}
