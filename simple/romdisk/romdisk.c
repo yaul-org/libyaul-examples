82e08aa4eea285fd5df9862985a524f166081cab
Israel Jacquez <mrkotfw@gmail.com>
2018-10-05 00:31:39 -0700

    Fix compilation for romdisk

--- simple/romdisk/romdisk.c
+++ simple/romdisk/romdisk.c
@@ -14,92 +14,111 @@
 
 extern uint8_t root_romdisk[];
 
-#define SYS_CHGSYSCK(_CkMode_) ((**(volatile void(**)(uint32_t))0x6000320)(_CkMode_))
-#define SYS_GETSYSCK (*(volatile uint32_t*)0x6000324)
+static void _vblank_in_handler(void);
 
-static void delay(uint16_t);
+static void _delay(uint16_t);
 
 int
 main(void)
 {
-        void *romdisk;
-
-        char *msg; /* Buffer for file */
-        ssize_t msg_len;
-        void *fh; /* File handle */
-
         vdp2_init();
         vdp2_scrn_back_screen_color_set(VRAM_ADDR_4MBIT(2, 0x01FFFE),
             COLOR_RGB555(0, 0, 7));
+
+        vdp2_sprite_type_set(0);
+        vdp2_sprite_priority_set(0, 0);
+
+        scu_ic_mask_chg(IC_MASK_ALL, IC_MASK_VBLANK_IN);
+        scu_ic_ihr_set(IC_INTERRUPT_VBLANK_IN, _vblank_in_handler);
+        scu_ic_mask_chg(~(IC_MASK_VBLANK_IN), IC_MASK_NONE);
+
+        cpu_intc_mask_set(0);
+
+        vdp2_tvmd_display_set();
 
         smpc_init();
 
         cons_init(CONS_DRIVER_VDP2, 40, 28);
 
-        cons_write("\n[1;44m          *** ROMDISK Test ***          [m\n\n");
-
-        cons_write("Mounting ROMDISK... ");
+        cons_buffer("\n[1;44m          *** ROMDISK Test ***          [m\n\n");
+        cons_buffer("Mounting ROMDISK... ");
 
         romdisk_init();
 
+        void *romdisk;
         romdisk = romdisk_mount("/", root_romdisk);
 
-        delay(1);
+        _delay(1);
 
         if (romdisk != NULL) {
-                cons_write("OK!\n");
+                cons_buffer("OK!\n");
         }
 
-        delay(1);
+        _delay(1);
 
-        cons_write("Opening \"/tmp/txt/hello.world\"... ");
+        cons_buffer("Opening \"/tmp/txt/hello.world\"... ");
+
+        void *fh;
         if ((fh = romdisk_open(romdisk, "/tmp/txt/hello.world")) == NULL) {
-                cons_write("[1;31mFAILED[0m\n");
+                cons_buffer("[1;31mFAILED[0m\n");
                 abort();
         }
 
-        delay(1);
+        _delay(1);
 
-        cons_write("OK!\n");
+        cons_buffer("OK!\n");
 
+        ssize_t msg_len;
         msg_len = romdisk_total(fh);
+
+        char *msg;
         msg = (char *)malloc(msg_len + 1);
         assert(msg != NULL);
         memset(msg, '\0', msg_len + 1);
 
-        delay(1);
+        _delay(1);
 
-        cons_write("Reading... ");
+        cons_buffer("Reading... ");
         msg_len = romdisk_read(fh, msg, romdisk_total(fh));
 
-        delay(1);
+        _delay(1);
 
-        cons_write("OK!\n");
+        cons_buffer("OK!\n");
 
         if (msg_len == 0) {
-                cons_write("FAILED\n");
+                cons_buffer("FAILED\n");
                 abort();
         }
 
-        cons_write("\n[1;32m");
-        cons_write(msg);
-        cons_write("[m");
+        cons_buffer("\n[1;32m");
+        cons_buffer(msg);
+        cons_buffer("[m");
 
-        cons_write("\nTest complete!\n");
+        cons_buffer("\nTest complete!\n");
 
         while (true) {
                 vdp2_tvmd_vblank_out_wait();
                 vdp2_tvmd_vblank_in_wait();
+                cons_flush();
+                vdp2_commit();
         }
 }
 
 static void
-delay(uint16_t t)
+_delay(uint16_t t)
 {
         uint16_t frame;
 
         for (frame = 0; frame < (60 * t); frame++) {
                 vdp2_tvmd_vblank_out_wait();
                 vdp2_tvmd_vblank_in_wait();
+                cons_flush();
+                vdp2_commit();
         }
+}
+
+static void
+_vblank_in_handler(void)
+{
+        dma_queue_flush(DMA_QUEUE_TAG_VBLANK_IN);
 }
