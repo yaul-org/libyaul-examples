a16684b52cb3e1f768a79bc319ff7eefea365c80
Israel Jacquez <mrkotfw@gmail.com>
2018-10-04 23:32:46 -0700

    Fix compilation for cpu-frt

--- simple/cpu-frt/cpu-frt.c
+++ simple/cpu-frt/cpu-frt.c
@@ -10,9 +10,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FRT_INTERRUPT_PRIORITY_LEVEL    8
+#define FRT_INTERRUPT_PRIORITY_LEVEL 8
 
-#define TIMER_MAX_TIMERS_COUNT      16
+#define TIMER_MAX_TIMERS_COUNT 16
 
 struct timer;
 
@@ -43,7 +43,116 @@ static struct timer_state _timer_states[TIMER_MAX_TIMERS_COUNT];
 static uint32_t _next_timer = 0;
 static uint32_t _id_count = 0;
 
-static void __unused
+static volatile uint32_t _ovi_count = 0;
+static volatile uint32_t _ocb_count = 0;
+
+static void _timer_init(void);
+static int32_t _timer_add(const struct timer *);
+static int32_t _timer_remove(uint32_t) __unused;
+
+static void _frt_compare_output_handler(void);
+static void _frt_ovi_handler(void);
+static void _frt_ocb_handler(void);
+static void _vblank_in_handler(void);
+static void _timer_handler(struct timer_event *);
+
+static void _hardware_init(void);
+
+static char _buffer[256];
+
+static volatile uint32_t _counter_1 = 0;
+static volatile uint32_t _counter_2 = 0;
+static volatile uint32_t _counter_3 = 0;
+static volatile uint32_t _counter_4 = 0;
+
+void
+main(void)
+{
+        _hardware_init();
+
+        cons_init(CONS_DRIVER_VDP2, 40, 28);
+
+        _timer_init();
+
+        struct timer match1 __unused = {
+                .interval = 1000,
+                .callback = _timer_handler,
+                .work = (void *)&_counter_1
+        };
+
+        struct timer match2 __unused = {
+                .interval = 2000,
+                .callback = _timer_handler,
+                .work = (void *)&_counter_2
+        };
+
+        struct timer match3 __unused = {
+                .interval = 3,
+                .callback = _timer_handler,
+                .work = (void *)&_counter_3
+        };
+
+        struct timer match4 __unused = {
+                .interval = 500,
+                .callback = _timer_handler,
+                .work = (void *)&_counter_4
+        };
+
+        _timer_add(&match1);
+        _timer_add(&match2);
+        _timer_add(&match3);
+        _timer_add(&match4);
+
+        while (true) {
+                vdp2_tvmd_vblank_out_wait();
+
+                cons_buffer("[1;1H[2J");
+
+                (void)sprintf(_buffer, "\n"
+                    " counter_1: %10lu (1s)\n"
+                    " counter_2: %10lu (2s)\n"
+                    " counter_3: %10lu (3ms)\n"
+                    " counter_4: %10lu (.5s)\n"
+                    " ovi_count: %10lu\n"
+                    " ocb_count: %10lu\n",
+                    _counter_1,
+                    _counter_2,
+                    _counter_3,
+                    _counter_4,
+                    _ovi_count,
+                    _ocb_count);
+                cons_buffer(_buffer);
+
+                vdp2_tvmd_vblank_in_wait();
+                cons_flush();
+                vdp2_commit();
+        }
+}
+
+static void
+_hardware_init(void)
+{
+        vdp2_init();
+
+        vdp2_tvmd_display_res_set(TVMD_INTERLACE_NONE, TVMD_HORZ_NORMAL_A,
+            TVMD_VERT_224);
+
+        vdp2_sprite_type_set(0);
+        vdp2_sprite_priority_set(0, 0);
+
+        vdp2_scrn_back_screen_color_set(VRAM_ADDR_4MBIT(3, 0x01FFFE),
+            COLOR_RGB555(0, 3, 3));
+
+        scu_ic_mask_chg(IC_MASK_ALL, IC_MASK_VBLANK_IN);
+        scu_ic_ihr_set(IC_INTERRUPT_VBLANK_IN, _vblank_in_handler);
+        scu_ic_mask_chg(~(IC_MASK_VBLANK_IN), IC_MASK_NONE);
+
+        cpu_intc_mask_set(0);
+
+        vdp2_tvmd_display_set();
+}
+
+static void
 _frt_compare_output_handler(void)
 {
         uint32_t frt_count;
@@ -93,23 +202,8 @@ _frt_compare_output_handler(void)
         }
 }
 
-static volatile uint32_t _ovi_count = 0;
-static volatile uint32_t _ocb_count = 0;
-
-static void __unused
-_frt_ovi_handler(void)
-{
-        _ovi_count++;
-}
-
-static void __unused
-_frt_ocb_handler(void)
-{
-        _ocb_count++;
-}
-
-void
-timer_init(void)
+static void
+_timer_init(void)
 {
         uint32_t timer;
         for (timer = 0; timer < TIMER_MAX_TIMERS_COUNT; timer++) {
@@ -125,14 +219,14 @@ timer_init(void)
         cpu_frt_interrupt_priority_set(FRT_INTERRUPT_PRIORITY_LEVEL);
 }
 
-int32_t
-timer_add(const struct timer *timer)
+static int32_t
+_timer_add(const struct timer *timer)
 {
         /* Disable interrupts */
         uint32_t i_mask;
         i_mask = cpu_intc_mask_get();
 
-        cpu_intc_mask_set(0x0F);
+        cpu_intc_mask_set(15);
 
         if (timer->callback == NULL) {
                 return -1;
@@ -179,8 +273,8 @@ timer_add(const struct timer *timer)
         return 0;
 }
 
-int32_t
-timer_remove(uint32_t id)
+static int32_t
+_timer_remove(uint32_t id)
 {
         int32_t ret;
 
@@ -188,7 +282,7 @@ timer_remove(uint32_t id)
         uint32_t i_mask;
         i_mask = cpu_intc_mask_get();
 
-        cpu_intc_mask_set(0x0F);
+        cpu_intc_mask_set(15);
 
         struct timer_state *timer_state;
 
@@ -215,131 +309,27 @@ timer_remove(uint32_t id)
         ret = 0;
 
 exit:
-        /* Enable interrupts */
         cpu_intc_mask_set(i_mask);
 
         return ret;
 }
-////////////////////////////////////////////////////////////////////////////////
-static void _vblank_in_handler(irq_mux_handle_t *);
-static void _vblank_out_handler(irq_mux_handle_t *);
-static void _timer_handler(struct timer_event *);
 
-static void _hardware_init(void);
-
-static char _buffer[256];
-
-static volatile uint32_t _counter_1 = 0;
-static volatile uint32_t _counter_2 = 0;
-static volatile uint32_t _counter_3 = 0;
-static volatile uint32_t _counter_4 = 0;
-
-void
-main(void)
+static void
+_frt_ovi_handler(void)
 {
-        _hardware_init();
-
-        cons_init(CONS_DRIVER_VDP2, 40, 28);
-
-        timer_init();
-
-        struct timer match1 __unused = {
-                .interval = 1000,
-                .callback = _timer_handler,
-                .work = (void *)&_counter_1
-        };
-
-        struct timer match2 __unused = {
-                .interval = 2000,
-                .callback = _timer_handler,
-                .work = (void *)&_counter_2
-        };
-
-        struct timer match3 __unused = {
-                .interval = 3,
-                .callback = _timer_handler,
-                .work = (void *)&_counter_3
-        };
-
-        struct timer match4 __unused = {
-                .interval = 500,
-                .callback = _timer_handler,
-                .work = (void *)&_counter_4
-        };
-
-        timer_add(&match1);
-        timer_add(&match2);
-        timer_add(&match3);
-        timer_add(&match4);
-
-        while (true) {
-                vdp2_tvmd_vblank_out_wait();
-
-                cons_buffer("[1;1H[2J");
-
-                (void)sprintf(_buffer, "\n"
-                    " counter_1: %10lu (1s)\n"
-                    " counter_2: %10lu (2s)\n"
-                    " counter_3: %10lu (3ms)\n"
-                    " counter_4: %10lu (.5s)\n"
-                    " ovi_count: %10lu\n"
-                    " ocb_count: %10lu\n",
-                    _counter_1,
-                    _counter_2,
-                    _counter_3,
-                    _counter_4,
-                    _ovi_count,
-                    _ocb_count);
-                cons_buffer(_buffer);
-
-                vdp2_tvmd_vblank_in_wait();
-                cons_flush();
-        }
+        _ovi_count++;
 }
 
 static void
-_hardware_init(void)
+_frt_ocb_handler(void)
 {
-        vdp2_init();
-
-        vdp2_tvmd_display_res_set(TVMD_INTERLACE_NONE, TVMD_HORZ_NORMAL_A,
-            TVMD_VERT_224);
-
-        vdp2_sprite_priority_set(0, 0);
-        vdp2_sprite_priority_set(1, 0);
-        vdp2_sprite_priority_set(2, 0);
-        vdp2_sprite_priority_set(3, 0);
-        vdp2_sprite_priority_set(4, 0);
-        vdp2_sprite_priority_set(5, 0);
-        vdp2_sprite_priority_set(6, 0);
-        vdp2_sprite_priority_set(7, 0);
-
-        vdp2_scrn_back_screen_color_set(VRAM_ADDR_4MBIT(3, 0x01FFFE),
-            COLOR_RGB555(0, 3, 3));
-
-        irq_mux_t *vblank_in;
-        vblank_in = vdp2_tvmd_vblank_in_irq_get();
-        irq_mux_handle_add(vblank_in, _vblank_in_handler, NULL);
-
-        irq_mux_t *vblank_out;
-        vblank_out = vdp2_tvmd_vblank_out_irq_get();
-        irq_mux_handle_add(vblank_out, _vblank_out_handler, NULL);
-
-        /* Enable interrupts */
-        cpu_intc_mask_set(0x7);
-
-        vdp2_tvmd_display_set();
+        _ocb_count++;
 }
 
 static void
-_vblank_in_handler(irq_mux_handle_t *irq_mux __unused)
+_vblank_in_handler(void)
 {
-        vdp2_commit();
-}
-
-static void
-_vblank_out_handler(irq_mux_handle_t *irq_mux __unused)
-{
+        dma_queue_flush(DMA_QUEUE_TAG_VBLANK_IN);
 }
 
 static void
