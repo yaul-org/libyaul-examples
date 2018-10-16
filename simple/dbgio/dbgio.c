4096d052c4bf5c8bc005d56505c328be1068a177
Israel Jacquez <mrkotfw@gmail.com>
2018-10-15 18:13:23 -0700

    Add dbgio example

---
+++ simple/dbgio/dbgio.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2012-2016 Israel Jacquez
+ * See LICENSE for details.
+ *
+ * Israel Jacquez <mrkotfw@gmail.com>
+ */
+
+#include <yaul.h>
+
+#include <stdio.h>
+
+static void _hardware_init(void);
+
+static void _ovi_handler(void);
+
+static volatile uint32_t _ovf_count = 0;
+
+/* Transfer 512 KiB */
+static uint8_t _copy_buffer[512 << 10];
+
+int
+main(void)
+{
+        _hardware_init();
+
+        cons_init(CONS_DRIVER_VDP2, 40, 28);
+
+        (void)memset(_copy_buffer, '*', sizeof(_copy_buffer));
+
+        cons_buffer("Using f/8 CPU FRT divisor\n\n");
+
+        char buffer[256];
+
+        uint16_t before;
+        uint16_t after;
+        uint32_t count;
+
+        uint32_t i;
+
+        cpu_frt_count_set(0);
+        _ovf_count = 0;
+
+        before = cpu_frt_count_get();
+        for (i = 0; i < sizeof(_copy_buffer); i++) {
+                usb_cart_byte_send(_copy_buffer[i]);
+        }
+        after = cpu_frt_count_get();
+        count = (after - before) + (65535 * _ovf_count);
+
+        (void)sprintf(buffer, "Using CPU byte transfer:\n%16lu FRT ticks\n", count);
+        cons_buffer(buffer);
+
+        cpu_frt_count_set(0);
+        _ovf_count = 0;
+
+        before = cpu_frt_count_get();
+        usb_cart_dma_send(buffer, sizeof(_copy_buffer));
+        after = cpu_frt_count_get();
+        count = (after - before) + (65535 * _ovf_count);
+
+        (void)sprintf(buffer, "Using CPU-DMAC:\n%16lu FRT ticks\n", count);
+        cons_buffer(buffer);
+
+        cons_flush();
+        vdp2_sync_commit();
+        vdp_sync(0);
+
+        while (true) {
+        }
+
+        return 0;
+}
+
+static void
+_hardware_init(void)
+{
+        vdp2_tvmd_display_res_set(TVMD_INTERLACE_NONE, TVMD_HORZ_NORMAL_A,
+            TVMD_VERT_224);
+
+        vdp2_scrn_back_screen_color_set(VRAM_ADDR_4MBIT(3, 0x01FFFE),
+            COLOR_RGB555(0, 3, 15));
+
+        vdp2_tvmd_display_set();
+
+        cpu_frt_init(FRT_CLOCK_DIV_8);
+        cpu_frt_ovi_set(_ovi_handler);
+
+        cpu_intc_mask_set(0);
+}
+
+static void
+_ovi_handler(void)
+{
+        _ovf_count++;
+}
