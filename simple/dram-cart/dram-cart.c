6abc3bb78f2dd0e77cfc32da23e5fd83f973b37f
Israel Jacquez <mrkotfw@gmail.com>
2018-11-02 09:10:00 -0700

    Rename and refactor dram-cartridge example to dram-cart

---
+++ simple/dram-cart/dram-cart.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2012-2016 Israel Jacquez
+ * See LICENSE for details.
+ *
+ * Israel Jacquez <mrkotfw@gmail.com>
+ */
+
+#include <yaul.h>
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+static void _hardware_init(void);
+
+static const char *_error_message =
+    "[1;4H[2K[11CThe extended RAM\n"
+    "[11Ccartridge is not\n"
+    "[11Cinserted properly.\n"
+    "\n"
+    "[11CPlease turn off\n"
+    "[11Cpower and re-insert\n"
+    "[11Cthe extended RAM\n"
+    "[11Ccartridge.\n";
+
+int
+main(void)
+{
+        _hardware_init();
+
+        dbgio_dev_default_init(DBGIO_DEV_VDP2);
+        dbgio_dev_set(DBGIO_DEV_VDP2);
+
+        uint32_t id;
+        id = dram_cart_id_get();
+
+        if ((id != DRAM_CART_ID_1MIB) && (id != DRAM_CART_ID_4MIB)) {
+                dbgio_buffer(_error_message);
+                dbgio_flush();
+                vdp2_sync_commit();
+                vdp_sync(0);
+                abort();
+        }
+
+        char buffer[64];
+
+        uint32_t *cart_area __unused;
+        cart_area = (uint32_t *)dram_cart_area_get();
+
+        size_t cart_len;
+        cart_len = dram_cart_size_get();
+
+        char *type_str = ((id == DRAM_CART_ID_1MIB)
+            ? "8-Mbit"
+            : "32-Mbit");
+
+        (void)sprintf(buffer, "%s DRAM Cartridge detected (%i bytes)\n",
+            type_str, cart_len);
+        dbgio_buffer(buffer);
+
+        vdp2_sync_commit();
+        vdp_sync(0);
+
+        while (true) {
+        }
+}
+
+static void
+_hardware_init(void)
+{
+        vdp2_tvmd_display_res_set(TVMD_INTERLACE_NONE, TVMD_HORZ_NORMAL_A,
+            TVMD_VERT_224);
+
+        vdp2_scrn_back_screen_color_set(VRAM_ADDR_4MBIT(3, 0x01FFFE),
+            COLOR_RGB555(0, 3, 3));
+
+        cpu_intc_mask_set(0);
+
+        vdp2_tvmd_display_set();
+}
