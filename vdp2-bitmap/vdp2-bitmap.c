2e3780fca46f79cf432844c2d6a98ea95a189d08
Israel Jacquez <mrkotfw@gmail.com>
2019-02-12 09:53:23 -0800

    Clean up vdp2-bitmap to compile on GCC 4.7.4

--- vdp2-bitmap/vdp2-bitmap.c
+++ vdp2-bitmap/vdp2-bitmap.c
@@ -9,9 +9,12 @@
 
 #include <tga.h>
 
-extern uint8_t root_romdisk[];
+#define RBG0_BPD                VRAM_ADDR_4MBIT(0, 0x00000)
+#define RBG0_ROTATION_TABLE     VRAM_ADDR_4MBIT(2, 0x00000)
 
-static void _vblank_in_handler(void);
+#define BACK_SCREEN             VRAM_ADDR_4MBIT(3, 0x1FFFE)
+
+extern uint8_t root_romdisk[];
 
 static void _hardware_init(void);
 
@@ -35,7 +38,7 @@ main(void)
         ret = tga_read(&tga, romdisk_direct(fh));
         assert(ret == TGA_FILE_OK);
 
-        (void)tga_image_decode(&tga, (void *)VRAM_ADDR_4MBIT(0, 0x00000));
+        (void)tga_image_decode(&tga, (void *)RBG0_BPD);
 
         struct scrn_rotation_table rot_tbl = {
                 /* Screen start coordinates */
@@ -52,12 +55,16 @@ main(void)
                 .delta_y = 0x00000000,
 
                 /* Rotation matrix */
-                .matrix.param.a = 0x00010000,
-                .matrix.param.b = 0x00000000,
-                .matrix.param.c = 0x00000000,
-                .matrix.param.d = 0x00000000,
-                .matrix.param.e = 0x00010000,
-                .matrix.param.f = 0x00000000,
+                .matrix = {
+                        .param = {
+                                .a = 0x00010000,
+                                .b = 0x00000000,
+                                .c = 0x00000000,
+                                .d = 0x00000000,
+                                .e = 0x00010000,
+                                .f = 0x00000000
+                        }
+                },
 
                 /* View point coordinates */
                 .px = 0,
@@ -89,7 +96,7 @@ main(void)
         struct dma_reg_buffer reg_buffer;
 
         dma_level_cfg.dlc_xfer.direct.len = sizeof(struct scrn_rotation_table);
-        dma_level_cfg.dlc_xfer.direct.dst = VRAM_ADDR_4MBIT(2, 0x00000);
+        dma_level_cfg.dlc_xfer.direct.dst = RBG0_ROTATION_TABLE;
         dma_level_cfg.dlc_xfer.direct.src = (uint32_t)&rot_tbl;
         dma_level_cfg.dlc_mode = DMA_MODE_DIRECT;
         dma_level_cfg.dlc_stride = DMA_STRIDE_2_BYTES;
@@ -120,24 +127,27 @@ main(void)
 static void
 _hardware_init(void)
 {
-        struct scrn_bitmap_format format;
-        memset(&format, 0x00, sizeof(format));
-
-        format.sbf_scroll_screen = SCRN_RBG0;
-        format.sbf_cc_count = SCRN_CCC_RGB_32768;
-        format.sbf_bitmap_size.width = 512;
-        format.sbf_bitmap_size.height = 256;
-        format.sbf_color_palette = 0x00000000;
-        format.sbf_bitmap_pattern = VRAM_ADDR_4MBIT(0, 0x00000);
-        format.sbf_rp_mode = 0;
-        format.sbf_sf_type = SCRN_SF_TYPE_NONE;
-        format.sbf_sf_code = SCRN_SF_CODE_A;
-        format.sbf_sf_mode = 0;
-        format.sbf_rotation_table = VRAM_ADDR_4MBIT(2, 0x00000);
-        format.sbf_usage_banks.a0 = VRAM_USAGE_TYPE_BPD;
-        format.sbf_usage_banks.a1 = VRAM_USAGE_TYPE_BPD;
-        format.sbf_usage_banks.b0 = VRAM_USAGE_TYPE_NONE;
-        format.sbf_usage_banks.b1 = VRAM_USAGE_TYPE_NONE;
+        const struct scrn_bitmap_format format = {
+                .sbf_scroll_screen = SCRN_RBG0,
+                .sbf_cc_count = SCRN_CCC_RGB_32768,
+                .sbf_bitmap_size = {
+                        512,
+                        256
+                },
+                .sbf_color_palette = 0x00000000,
+                .sbf_bitmap_pattern = RBG0_BPD,
+                .sbf_rp_mode = 0,
+                .sbf_sf_type = SCRN_SF_TYPE_NONE,
+                .sbf_sf_code = SCRN_SF_CODE_A,
+                .sbf_sf_mode = 0,
+                .sbf_rotation_table = RBG0_ROTATION_TABLE,
+                .sbf_usage_banks = {
+                        .a0 = VRAM_USAGE_TYPE_BPD,
+                        .a1 = VRAM_USAGE_TYPE_BPD,
+                        .b0 = VRAM_USAGE_TYPE_NONE,
+                        .b1 = VRAM_USAGE_TYPE_NONE
+                }
+        };
 
         vdp2_scrn_bitmap_format_set(&format);
         vdp2_scrn_priority_set(SCRN_RBG0, 7);
@@ -145,10 +155,7 @@ _hardware_init(void)
 
         vdp2_vram_cycp_clear();
 
-        vdp_sync_vblank_in_set(_vblank_in_handler);
-
-        vdp2_scrn_back_screen_color_set(VRAM_ADDR_4MBIT(3, 0x01FFFE),
-                                        COLOR_RGB555(5, 5, 7));
+        vdp2_scrn_back_screen_color_set(BACK_SCREEN, COLOR_RGB555(5, 5, 7));
 
         vdp2_sprite_priority_set(0, 0);
         vdp2_sprite_priority_set(1, 0);
@@ -162,9 +169,4 @@ _hardware_init(void)
         vdp2_tvmd_display_res_set(TVMD_INTERLACE_NONE, TVMD_HORZ_NORMAL_A,
             TVMD_VERT_224);
         vdp2_tvmd_display_set();
-}
-
-static void
-_vblank_in_handler(void)
-{
 }
