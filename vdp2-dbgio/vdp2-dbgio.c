d01f841ca7dc775808da1d8a123f76955264f037
Israel Jacquez <mrkotfw@gmail.com>
2019-07-05 17:25:50 -0700

    Add tests to vdp2-dbgio

--- vdp2-dbgio/vdp2-dbgio.c
+++ vdp2-dbgio/vdp2-dbgio.c
@@ -8,10 +8,14 @@
 #include <yaul.h>
 
 #include <assert.h>
+#include <stdlib.h>
 
 static void _hardware_init(void);
 
-static void _test_csi_action(const char);
+static void _test_csi_action(const char) __used;
+static void _test_buffer_overflow(void) __used;
+static void _test_cursor_position(void) __used;
+static void _test_clearing(void) __used;
 
 int
 main(void)
@@ -20,12 +24,10 @@ main(void)
 
         dbgio_dev_default_init(DBGIO_DEV_VDP2_ASYNC);
 
-        _test_csi_action('H');
-
-        dbgio_flush();
-        vdp_sync(0);
-
-        gdb_init();
+        /* _test_csi_action('H'); */
+        _test_buffer_overflow();
+        /* _test_cursor_position(); */
+        /* _test_clearing(); */
 
         while (true) {
         }
@@ -64,4 +66,54 @@ _test_csi_action(const char ch)
                         _test_csi_action_h();
                         break;
         }
+}
+
+static void
+_test_buffer_overflow(void)
+{
+        char *large_buffer;
+        large_buffer = malloc(2048);
+        assert(large_buffer != NULL);
+
+        (void)memset(large_buffer, 'x', 2048);
+        large_buffer[2047] = '\0';
+
+        /* Completely overflow the screen */
+        dbgio_buffer(large_buffer);
+        /* Test if we can move the cursor back to the top left */
+        dbgio_buffer("[1;1HBuffer overflow averted\n");
+
+        free(large_buffer);
+
+        while (true) {
+                dbgio_flush();
+                vdp_sync(0);
+        }
+}
+
+static void
+_test_cursor_position(void)
+{
+        /* Move the cursor back to the top left and move the cursor down */
+        dbgio_buffer("[H\n");
+        for (uint32_t i = 0; i < 1024; i++) {
+                dbgio_buffer("\n");
+        }
+        dbgio_buffer("Exceeded screen bounds\n");
+        dbgio_buffer("[1;1HCursor out of bounds averted\n");
+
+        /* Allow setting the cursor beyond the bounds of the screen */
+        dbgio_buffer("[50;50H");
+        dbgio_buffer("Exceeded cons screen bounds\n");
+        dbgio_buffer("[2;1HCursor out of bounds averted\n");
+
+        while (true) {
+                dbgio_flush();
+                vdp_sync(0);
+        }
+}
+
+static void
+_test_clearing(void)
+{
 }
