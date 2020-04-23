6067cfa939e904d8f2ea7630af84e90ee637ad2d
Israel Jacquez <mrkotfw@gmail.com>
2020-04-23 11:40:27 -0700

    Update vdp1-drawing to manipulating polygon

--- vdp1-drawing/vdp1-drawing.c
+++ vdp1-drawing/vdp1-drawing.c
@@ -13,69 +13,266 @@
 #define SCREEN_WIDTH    320
 #define SCREEN_HEIGHT   224
 
+#define STATE_IDLE              (-1)
+#define STATE_TYPE_SET          (0)
+#define STATE_DRAW_MODE_SET     (1)
+#define STATE_VERTEX_SELECT     (2)
+#define STATE_VERTEX_RESET      (3)
+
+#define PRIMITIVE_TYPE_POLYLINE   (0)
+#define PRIMITIVE_TYPE_POLYGON    (1)
+#define PRIMITIVE_TYPE_COUNT      (2)
+
+#define PRIMITIVE_DRAW_MODE_NORMAL                (0)
+#define PRIMITIVE_DRAW_MODE_MESH                  (1)
+#define PRIMITIVE_DRAW_MODE_SHADOW                (2)
+#define PRIMITIVE_DRAW_MODE_HALF_LUMINANCE        (3)
+#define PRIMITIVE_DRAW_MODE_HALF_TRANSPARENT      (4)
+#define PRIMITIVE_DRAW_MODE_GOURAUD_SHADING       (5)
+#define PRIMITIVE_DRAW_MODE_GOURAUD_HALF_LUM      (6)
+#define PRIMITIVE_DRAW_MODE_GOURAUD_HALF_TRANS    (7)
+#define PRIMITIVE_DRAW_MODE_COUNT                 (8)
+
+#define PRIMITIVE_WIDTH           32
+#define PRIMITIVE_HEIGHT          32
+#define PRIMITIVE_HALF_WIDTH      (PRIMITIVE_WIDTH / 2)
+#define PRIMITIVE_HALF_HEIGHT     (PRIMITIVE_HEIGHT / 2)
+
+#define PRIMITIVE_COLOR           COLOR_RGB555(31, 0, 31)
+
+#define ORDER_SYSTEM_CLIP_COORDS_INDEX  0
+#define ORDER_LOCAL_COORDS_INDEX        1
+#define ORDER_POLYGON_INDEX             2
+#define ORDER_DRAW_END_INDEX            3
+#define ORDER_COUNT                     4
+
+static struct smpc_peripheral_digital _digital;
+
+static struct vdp1_cmdt_list *_cmdt_list = NULL;
+
+static struct {
+        int8_t type;
+        int8_t draw_mode;
+        color_rgb555_t color;
+        int16_vector2_t points[4];
+} _primitive;
+
+static uint32_t _state = STATE_IDLE;
+
+static vdp1_cmdt_draw_mode _primitive_draw_modes[] = {
+        {
+                .raw = 0x0000
+        },
+        {
+                .raw = 0x0000,
+                .bits.mesh_enable = true
+        },
+        {
+                .raw = 0x0000,
+                .bits.cc_mode = 1
+        },
+        {
+                .raw = 0x0000,
+                .bits.cc_mode = 2
+        },
+        {
+                .raw = 0x0000,
+                .bits.cc_mode = 3
+        },
+        {
+                .raw = 0x0000,
+                .bits.cc_mode = 4
+        },
+        {
+                .raw = 0x0000,
+                .bits.cc_mode = 6
+        },
+        {
+                .raw = 0x0000,
+                .bits.cc_mode = 7
+        }
+};
+
+static char *_primitive_type_strings[] = {
+        "POLYLINE",
+        "POLYGON"
+};
+
+static char *_primitive_draw_mode_strings[] = {
+        "NORMAL",
+        "MESH",
+        "SHADOW",
+        "HALF-LUMINANCE",
+        "REPLACE/HALF-TRANSPARENT",
+        "GOURAUD",
+        "GOURAUD+HALF-LUMINANCE",
+        "GOURAUD+HALF-TRANSPARENT"
+};
+
 static void _hardware_init(void);
 
-static void _setup_drawing_env(struct vdp1_cmdt_list *, bool);
-static void _setup_clear_fb(struct vdp1_cmdt_list *, const color_rgb555_t, bool);
+static void _cmdt_list_init(void);
+static void _primitive_init(void);
+
+static void _vblank_out_handler(void);
 
 void
 main(void)
 {
+        const uint16_t mask_pressed_buttons =
+            PERIPHERAL_DIGITAL_X |
+            PERIPHERAL_DIGITAL_Y |
+            PERIPHERAL_DIGITAL_B |
+            PERIPHERAL_DIGITAL_A;
+
+        const uint16_t mask_pressed_type =
+            PERIPHERAL_DIGITAL_LEFT |
+            PERIPHERAL_DIGITAL_RIGHT;
+
+        const uint16_t mask_pressed_draw_mode =
+            PERIPHERAL_DIGITAL_UP |
+            PERIPHERAL_DIGITAL_DOWN;
+
         _hardware_init();
 
-        struct vdp1_cmdt_list *cmdt_lists[2];
+        dbgio_dev_default_init(DBGIO_DEV_VDP2_SIMPLE);
 
-        cmdt_lists[0] = vdp1_cmdt_list_alloc(5);
-
-        _setup_drawing_env(cmdt_lists[0], false);
-        _setup_clear_fb(cmdt_lists[0], COLOR_RGB555(31, 0, 0), true);
-
-        vdp1_sync_draw(cmdt_lists[0], NULL, NULL);
-
-        /* Process another list while drawing */
-
-        cmdt_lists[1] = vdp1_cmdt_list_alloc(2);
-
-        struct vdp1_cmdt_polygon polygon;
-
-        polygon.draw_mode.raw = 0x0000;
-        polygon.color = COLOR_RGB555(0, 31, 0);
-        polygon.vertex.a.x = 0;
-        polygon.vertex.a.y = (SCREEN_HEIGHT / 2) - 1;
-
-        polygon.vertex.b.x = (SCREEN_WIDTH / 2) - 1;
-        polygon.vertex.b.y = (SCREEN_HEIGHT / 2) - 1;
-
-        polygon.vertex.c.x = (SCREEN_WIDTH / 2) - 1;
-        polygon.vertex.c.y = 0;
-
-        polygon.vertex.d.x = 0;
-        polygon.vertex.d.y = 0;
-
-        vdp1_cmdt_polygon_add(cmdt_lists[1], &polygon);
-        vdp1_cmdt_end(cmdt_lists[1]);
-
-        vdp1_sync_draw(cmdt_lists[1], NULL, NULL);
-
-        vdp_sync(0);
-
-        vdp1_cmdt_list_free(cmdt_lists[0]);
-        vdp1_cmdt_list_free(cmdt_lists[1]);
+        _cmdt_list_init();
+        _primitive_init();
 
         while (true) {
+                smpc_peripheral_process();
+                smpc_peripheral_digital_port(1, &_digital);
+
+                uint32_t vertex_index = CMDT_VTX_POLYGON_A;
+
+                _state = STATE_IDLE;
+
+                const uint16_t pressed_state = _digital.pressed.raw;
+                const uint16_t held_state = _digital.held.raw;
+
+                if ((pressed_state & mask_pressed_buttons) != 0x0000) {
+                        _state = STATE_VERTEX_SELECT;
+                } else if ((pressed_state & mask_pressed_type) != 0x0000) {
+                        _state = STATE_TYPE_SET;
+                }  else if ((pressed_state & mask_pressed_draw_mode) != 0x0000) {
+                        _state = STATE_DRAW_MODE_SET;
+                } else if ((pressed_state & PERIPHERAL_DIGITAL_START) != 0x0000) {
+                        _state = STATE_VERTEX_RESET;
+                }
+
+                switch (_state) {
+                case STATE_VERTEX_RESET:
+                        _primitive_init();
+                        break;
+                case STATE_TYPE_SET:
+                        if ((held_state & PERIPHERAL_DIGITAL_LEFT) != 0x0000) {
+                                _primitive.type--;
+                        } else if ((held_state & PERIPHERAL_DIGITAL_RIGHT) != 0x0000) {
+                                _primitive.type++;
+                        }
+
+                        if (_primitive.type < 0) {
+                                _primitive.type = PRIMITIVE_TYPE_COUNT - 1;
+                        } else if (_primitive.type >= PRIMITIVE_TYPE_COUNT) {
+                                _primitive.type = 0;
+                        }
+                        break;
+                case STATE_DRAW_MODE_SET:
+                        if ((held_state & PERIPHERAL_DIGITAL_UP) != 0x0000) {
+                                _primitive.draw_mode--;
+                        } else if ((held_state & PERIPHERAL_DIGITAL_DOWN) != 0x0000) {
+                                _primitive.draw_mode++;
+                        }
+
+                        if (_primitive.draw_mode < 0) {
+                                _primitive.draw_mode = PRIMITIVE_DRAW_MODE_COUNT - 1;
+                        } else if (_primitive.draw_mode >= PRIMITIVE_DRAW_MODE_COUNT) {
+                                _primitive.draw_mode = 0;
+                        }
+                        break;
+                case STATE_VERTEX_SELECT:
+                        if ((pressed_state & PERIPHERAL_DIGITAL_X) != 0x0000) {
+                                vertex_index = CMDT_VTX_POLYGON_D;
+                        } else if ((pressed_state & PERIPHERAL_DIGITAL_Y) != 0x0000) {
+                                vertex_index = CMDT_VTX_POLYGON_C;
+                        } else if ((pressed_state & PERIPHERAL_DIGITAL_B) != 0x0000) {
+                                vertex_index = CMDT_VTX_POLYGON_B;
+                        } else if ((pressed_state & PERIPHERAL_DIGITAL_A) != 0x0000) {
+                                vertex_index = CMDT_VTX_POLYGON_A;
+                        }
+
+                        if ((pressed_state & PERIPHERAL_DIGITAL_LEFT) != 0) {
+                                _primitive.points[vertex_index].x--;
+                        }
+
+                        if ((pressed_state & PERIPHERAL_DIGITAL_RIGHT) != 0) {
+                                _primitive.points[vertex_index].x++;
+                        }
+
+                        if ((pressed_state & PERIPHERAL_DIGITAL_UP) != 0) {
+                                _primitive.points[vertex_index].y--;
+                        }
+
+                        if ((pressed_state & PERIPHERAL_DIGITAL_DOWN) != 0) {
+                                _primitive.points[vertex_index].y++;
+                        }
+                        break;
+                }
+
+                struct vdp1_cmdt *cmdt_polygon;
+                cmdt_polygon = &_cmdt_list->cmdts[ORDER_POLYGON_INDEX];
+
+                switch (_primitive.type) {
+                case PRIMITIVE_TYPE_POLYLINE:
+                        vdp1_cmdt_polyline_set(cmdt_polygon);
+                        break;
+                case PRIMITIVE_TYPE_POLYGON:
+                        vdp1_cmdt_polygon_set(cmdt_polygon);
+                        break;
+                }
+
+                char buffer[128];
+
+                (void)sprintf(buffer,
+                    "[H[2J"
+                    "     type: %s\n"
+                    "draw_mode: %s\n",
+                    _primitive_type_strings[_primitive.type],
+                    _primitive_draw_mode_strings[_primitive.draw_mode]);
+
+                dbgio_buffer(buffer);
+                dbgio_flush();
+
+                vdp1_cmdt_param_draw_mode_set(cmdt_polygon,
+                    _primitive_draw_modes[_primitive.draw_mode]);
+                vdp1_cmdt_param_vertices_set(cmdt_polygon, &_primitive.points[0]);
+
+                vdp1_sync_cmdt_list_put(_cmdt_list, NULL, NULL);
+                vdp_sync();
         }
 }
 
 static void
 _hardware_init(void)
 {
+        smpc_init();
+        smpc_peripheral_init();
+
         vdp2_tvmd_display_res_set(VDP2_TVMD_INTERLACE_NONE, VDP2_TVMD_HORZ_NORMAL_A,
             VDP2_TVMD_VERT_224);
-
         vdp2_scrn_back_screen_color_set(VDP2_VRAM_ADDR(3, 0x01FFFE),
             COLOR_RGB555(0, 3, 15));
-
         vdp2_sprite_priority_set(0, 6);
+
+        struct vdp1_env env;
+        vdp1_env_default_init(&env);
+
+        env.erase_color = COLOR_RGB555(0, 3, 15);
+
+        vdp1_env_set(&env);
+
+        vdp_sync_vblank_out_set(_vblank_out_handler);
 
         cpu_intc_mask_set(0);
 
@@ -83,66 +280,83 @@ _hardware_init(void)
 }
 
 static void
-_setup_drawing_env(struct vdp1_cmdt_list *cmdt_list, bool end)
+_cmdt_list_init(void)
 {
-        struct vdp1_cmdt_local_coord local_coord = {
-                .coord = {
-                        .x = 0,
-                        .y = 0
-                }
-        };
+        static const int16_vector2_t system_clip_coord =
+            INT16_VECTOR2_INITIALIZER(SCREEN_WIDTH - 1,
+                                      SCREEN_HEIGHT - 1);
 
-        struct vdp1_cmdt_system_clip_coord system_clip = {
-                .coord = {
-                        .x = SCREEN_WIDTH - 1,
-                        .y = SCREEN_HEIGHT - 1
-                }
-        };
+        _cmdt_list = vdp1_cmdt_list_alloc(ORDER_COUNT);
 
-        struct vdp1_cmdt_user_clip_coord user_clip = {
-                .coords = {
-                        {
-                                .x = 0,
-                                .y = 0
-                        },
-                        {
-                                .x = SCREEN_WIDTH - 1,
-                                .y = SCREEN_HEIGHT - 1
-                        }
-                }
-        };
+        (void)memset(&_cmdt_list->cmdts[0], 0x00,
+            sizeof(struct vdp1_cmdt) * ORDER_COUNT);
 
-        vdp1_cmdt_system_clip_coord_add(cmdt_list, &system_clip);
-        vdp1_cmdt_user_clip_coord_add(cmdt_list, &user_clip);
-        vdp1_cmdt_local_coord_add(cmdt_list, &local_coord);
+        _cmdt_list->count = ORDER_COUNT;
 
-        if (end) {
-                vdp1_cmdt_end(cmdt_list);
-        }
+        struct vdp1_cmdt *cmdts;
+        cmdts = &_cmdt_list->cmdts[0];
+
+        vdp1_cmdt_system_clip_coord_set(&cmdts[ORDER_SYSTEM_CLIP_COORDS_INDEX]);
+        vdp1_cmdt_param_vertex_set(&cmdts[ORDER_SYSTEM_CLIP_COORDS_INDEX],
+            CMDT_VTX_SYSTEM_CLIP,
+            &system_clip_coord);
+
+        vdp1_cmdt_end_set(&cmdts[ORDER_DRAW_END_INDEX]);
 }
 
 static void
-_setup_clear_fb(struct vdp1_cmdt_list *cmdt_list, const color_rgb555_t color, bool end)
+_primitive_init(void)
 {
-        struct vdp1_cmdt_polygon polygon;
+        static const int16_vector2_t local_coord_center =
+            INT16_VECTOR2_INITIALIZER((SCREEN_WIDTH / 2) - PRIMITIVE_HALF_WIDTH - 1,
+                                      (SCREEN_HEIGHT / 2) - PRIMITIVE_HALF_HEIGHT - 1);
 
-        polygon.draw_mode.raw = 0x0000;
-        polygon.color = color;
-        polygon.vertex.a.x = 0;
-        polygon.vertex.a.y = SCREEN_HEIGHT - 1;
+        _primitive.type = PRIMITIVE_TYPE_POLYLINE;
+        _primitive.draw_mode = PRIMITIVE_DRAW_MODE_NORMAL;
 
-        polygon.vertex.b.x = SCREEN_WIDTH - 1;
-        polygon.vertex.b.y = SCREEN_HEIGHT - 1;
+        _primitive.color = PRIMITIVE_COLOR;
 
-        polygon.vertex.c.x = SCREEN_WIDTH - 1;
-        polygon.vertex.c.y = 0;
+        _primitive.points[0].x = 0;
+        _primitive.points[0].y = PRIMITIVE_HEIGHT - 1;
 
-        polygon.vertex.d.x = 0;
-        polygon.vertex.d.y = 0;
+        _primitive.points[1].x = PRIMITIVE_WIDTH - 1;
+        _primitive.points[1].y = PRIMITIVE_HEIGHT - 1;
 
-        vdp1_cmdt_polygon_add(cmdt_list, &polygon);
+        _primitive.points[2].x = PRIMITIVE_WIDTH - 1;
+        _primitive.points[2].y = 0;
 
-        if (end) {
-                vdp1_cmdt_end(cmdt_list);
-        }
+        _primitive.points[3].x = 0;
+        _primitive.points[3].y = 0;
+
+        struct vdp1_cmdt *cmdt_local_coords;
+        cmdt_local_coords = &_cmdt_list->cmdts[ORDER_LOCAL_COORDS_INDEX];
+
+        vdp1_cmdt_local_coord_set(cmdt_local_coords);
+        vdp1_cmdt_param_vertex_set(cmdt_local_coords, CMDT_VTX_LOCAL_COORD,
+            &local_coord_center);
+
+        struct vdp1_cmdt *cmdt_polygon;
+        cmdt_polygon = &_cmdt_list->cmdts[ORDER_POLYGON_INDEX];
+
+        color_rgb555_t *gouraud_base =
+            (color_rgb555_t *)vdp1_vram_gouraud_base_get();
+
+        gouraud_base[0] = COLOR_RGB555(31,  0,  0);
+        gouraud_base[1] = COLOR_RGB555( 0, 31,  0);
+        gouraud_base[2] = COLOR_RGB555( 0,  0, 31);
+        gouraud_base[3] = COLOR_RGB555(31, 31, 31);
+
+        vdp1_cmdt_polyline_set(cmdt_polygon);
+        vdp1_cmdt_param_color_set(cmdt_polygon, _primitive.color);
+        vdp1_cmdt_param_draw_mode_set(cmdt_polygon,
+            _primitive_draw_modes[_primitive.draw_mode]);
+        vdp1_cmdt_param_vertices_set(cmdt_polygon, &_primitive.points[0]);
+
+        vdp1_cmdt_param_gouraud_base_set(cmdt_polygon, (uint32_t)gouraud_base);
+}
+
+static void
+_vblank_out_handler(void)
+{
+        smpc_peripheral_intback_issue();
 }
