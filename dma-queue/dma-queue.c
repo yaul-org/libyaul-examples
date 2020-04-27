d60aa41b0f32e62c29d1150eb78f207d1114edcc
Israel Jacquez <mrkotfw@gmail.com>
2020-04-27 14:35:33 -0700

    Update dma-queue

--- dma-queue/dma-queue.c
+++ dma-queue/dma-queue.c
@@ -30,9 +30,9 @@ static vdp2_scrn_bitmap_format_t _format = {
         .sf_mode = 0
 };
 
-/* Setting the unused timing cycles from 0xF to 0xE increased performance
+/* Setting the no-access timing cycles from 0xF to 0xE increased performance
  * tremendously. This may be exasperated by the fact that we're writing to VDP2
- * VRAM outside of VBLANK-IN. */
+ * VRAM outside of VBLANK-IN */
 static const vdp2_vram_cycp_t _vram_cycp = {
         {
                 {
@@ -88,18 +88,22 @@ static const char *_tga_files[] = {
         NULL
 };
 
+uint8_t _intermediate_buffer[512 * 256 * 2] __aligned(4) __unused;
+
 static void _hardware_init(void);
 static void _romdisk_init(void);
 
-static uint32_t _tga_file_decode(const char *, void *, void *) __unused;
+static void _dma_handler(const dma_queue_transfer_t *transfer);
+
+static uint32_t _tga_file_decode(const char *, void *, void *);
+
+static void _dma_queue_enqueue(void *, void *, const uint32_t);
 
 int
 main(void)
 {
         _hardware_init();
         _romdisk_init();
-
-        dbgio_dev_default_init(DBGIO_DEV_VDP2_SIMPLE);
 
         vdp2_scrn_priority_set(VDP2_SCRN_NBG0, 7);
         vdp2_scrn_display_set(VDP2_SCRN_NBG0, /* no_trans = */ false);
@@ -112,10 +116,6 @@ main(void)
         uint32_t bank;
         bank = 0;
 
-        uint8_t *intermediate_buffer;
-        intermediate_buffer = malloc(16384);
-        assert(intermediate_buffer != NULL);
-
         while (true) {
                 if (*current_file == NULL) {
                         current_file = &_tga_files[0];
@@ -124,14 +124,16 @@ main(void)
                 void *lwram;
                 lwram = (void *)LWRAM(0x00000000);
 
-                void *vram __unused;
+                void *vram;
                 vram = (void *)VDP2_VRAM_ADDR(2 * bank, 0x00000);
 
                 uint32_t intermediate_size __unused;
                 intermediate_size = _tga_file_decode(*current_file, lwram,
-                    intermediate_buffer);
+                    _intermediate_buffer);
 
-                (void)memcpy(vram, &intermediate_buffer[0], intermediate_size);
+                _dma_queue_enqueue(vram, _intermediate_buffer, intermediate_size);
+
+                dma_queue_flush(DMA_QUEUE_TAG_IMMEDIATE);
 
                 /* Set the back screen to its original color */
                 vdp2_scrn_back_screen_color_set(VDP2_VRAM_ADDR(2 * bank, 0x01FFFE),
@@ -149,6 +151,8 @@ main(void)
                 }
 
                 vdp2_sync_commit();
+
+                dma_queue_flush_wait();
 
                 current_file++;
                 bank ^= 1;
@@ -170,6 +174,8 @@ _hardware_init(void)
         vdp2_sprite_priority_set(5, 0);
         vdp2_sprite_priority_set(6, 0);
         vdp2_sprite_priority_set(7, 0);
+
+        vdp2_scrn_priority_set(VDP2_SCRN_NBG0, 6);
 
         vdp2_scrn_back_screen_color_set(VDP2_VRAM_ADDR(3, 0x01FFFE),
             COLOR_RGB1555(1, 7, 7, 7));
@@ -222,4 +228,30 @@ _tga_file_decode(const char *file, void *tga_buffer, void *buffer)
          *      return, but because we're dealing with 16-BPP TGA images, it's 2
          *      bytes per pixel */
         return 2 * tga_image_decode(&tga, buffer);
+}
+
+static void
+_dma_queue_enqueue(void *dst, void *src, const uint32_t size)
+{
+        static struct scu_dma_level_cfg dma_cfg = {
+                .mode = SCU_DMA_MODE_DIRECT,
+                .stride = SCU_DMA_STRIDE_2_BYTES,
+                .update = SCU_DMA_UPDATE_NONE
+        };
+
+        dma_cfg.xfer.direct.len = size;
+        dma_cfg.xfer.direct.dst = (uint32_t)dst;
+        dma_cfg.xfer.direct.src = CPU_CACHE_THROUGH | (uint32_t)src;
+
+        scu_dma_handle_t dma_handle;
+
+        scu_dma_config_buffer(&dma_handle, &dma_cfg);
+
+        dma_queue_enqueue(&dma_handle, DMA_QUEUE_TAG_IMMEDIATE, _dma_handler, NULL);
+}
+
+static void
+_dma_handler(const dma_queue_transfer_t *transfer)
+{
+        assert(transfer->status == DMA_QUEUE_STATUS_COMPLETE);
 }
