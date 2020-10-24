82f747efbfe204f939b61be47592a5da56747b66
Israel Jacquez <mrkotfw@gmail.com>
2020-10-23 23:33:21 -0700

    Clean up coding style

--- cdblock/block.h
+++ cdblock/block.h
@@ -5,47 +5,47 @@
  * Romulo Fernandes Machado Leitao <abra185@gmail.com>
  */
 
-#pragma once
+#ifndef _BLOCK_H_
+#define _BLOCK_H_
 
 #include <yaul.h>
 
 #define DEBUG_FS 0
 
-#define HASH_PRIME 31
-#define HASH_CUT_NUMBER 1000000009
-#define HASH_CHAR(X) ((X) - 31)
+#define HASH_PRIME      (31)
+#define HASH_CUT_NUMBER (1000000009)
+#define HASH_CHAR(x)    ((x) - 31)
 
 enum VolumeDescriptorTypes {
-  VD_BOOT_RECORD = 0,
-  VD_PRIMARY,
-  VD_SUPPLEMENTARY,
-  VD_PARTITION_DESCRIPTOR,
+        VD_BOOT_RECORD = 0,
+        VD_PRIMARY,
+        VD_SUPPLEMENTARY,
+        VD_PARTITION_DESCRIPTOR,
 
-  VD_SET_TERMINATOR = 0xFF
+        VD_SET_TERMINATOR = 0xFF
 };
 
-
-// Use b for big endian on saturn.
+/* Use b for big endian on saturn. */
 typedef struct {
-  uint32_t l;
-  uint32_t b;
+        uint32_t l;
+        uint32_t b;
 } __packed MultiEndianNumber32;
 
 typedef struct {
-  uint16_t l;
-  uint16_t b;
+        uint16_t l;
+        uint16_t b;
 } __packed MultiEndianNumber16;
 
 typedef struct {
-  uint8_t data[2048];
+        uint8_t data[2048];
 } __packed Sector;
 
 typedef struct {
-  uint8_t date[17];
+        uint8_t date[17];
 } __packed Date;
 
 typedef struct {
-  uint8_t date[7];
+        uint8_t date[7];
 } __packed RecordingDateTime;
 
 #define FLAG_CDBLOCK_HIDDEN               (1 << 0)
@@ -56,120 +56,120 @@ typedef struct {
 #define FLAG_CDBLOCK_CONTINUE_NEXT_EXTENT (1 << 7)
 
 typedef struct {
-  uint8_t length;
-  uint8_t extendedAttributeLength;
-  MultiEndianNumber32 extentLocation;
-  MultiEndianNumber32 extentLength;
-  RecordingDateTime recordingDateTime;
-  uint8_t flags;
-  uint8_t unitSizeInterleavedMode;
-  uint8_t gapSizeInterleavedMode;
-  MultiEndianNumber16 volumeSequenceNumber;
-  uint8_t identifierLength;
+        uint8_t length;
+        uint8_t extendedAttributeLength;
+        MultiEndianNumber32 extentLocation;
+        MultiEndianNumber32 extentLength;
+        RecordingDateTime recordingDateTime;
+        uint8_t flags;
+        uint8_t unitSizeInterleavedMode;
+        uint8_t gapSizeInterleavedMode;
+        MultiEndianNumber16 volumeSequenceNumber;
+        uint8_t identifierLength;
 } __packed DirectoryRecord;
 
 static inline bool __always_inline isDirectory(DirectoryRecord* rec) { return rec->flags & FLAG_CDBLOCK_DIRECTORY; }
 
 typedef struct {
-  DirectoryRecord root;
-  uint8_t identifier;
+        DirectoryRecord root;
+        uint8_t identifier;
 } __packed RootDirectoryRecord;
 
 typedef struct {
-  uint8_t type;
-  uint8_t identifier[5];
-  uint8_t version;
+        uint8_t type;
+        uint8_t identifier[5];
+        uint8_t version;
 } __packed VolumeDescriptorSetCommon;
 
 static inline bool __always_inline isTerminator(VolumeDescriptorSetCommon* sec) { return sec->type == VD_SET_TERMINATOR; }
 
 typedef struct {
-  VolumeDescriptorSetCommon common;
-  uint8_t data[2041];
+        VolumeDescriptorSetCommon common;
+        uint8_t data[2041];
 } __packed VolumeDescriptorSet;
 
 typedef struct {
-  VolumeDescriptorSetCommon common;
-  uint8_t unused;
-  char systemIdentifier[32];
-  char volumeIdentifier[32];
-  uint8_t unused2[8];
+        VolumeDescriptorSetCommon common;
+        uint8_t unused;
+        char systemIdentifier[32];
+        char volumeIdentifier[32];
+        uint8_t unused2[8];
 
-  MultiEndianNumber32 volumeSpaceSize;
-  uint8_t unused3[32];
+        MultiEndianNumber32 volumeSpaceSize;
+        uint8_t unused3[32];
 
-  MultiEndianNumber16 volumeSetSize;
-  MultiEndianNumber16 volumeSequenceNumber;
-  MultiEndianNumber16 logicalBlockSize;
-  MultiEndianNumber32 pathTableSize;
+        MultiEndianNumber16 volumeSetSize;
+        MultiEndianNumber16 volumeSequenceNumber;
+        MultiEndianNumber16 logicalBlockSize;
+        MultiEndianNumber32 pathTableSize;
 
-  int32_t locationPathTableLittle;
-  int32_t locationOptionalPathTableLittle;
+        int32_t locationPathTableLittle;
+        int32_t locationOptionalPathTableLittle;
 
-  int32_t locationPathTableBig;
-  int32_t locationOptionalPathTableBig;
+        int32_t locationPathTableBig;
+        int32_t locationOptionalPathTableBig;
 
-  RootDirectoryRecord rootDirectoryRecord;
+        RootDirectoryRecord rootDirectoryRecord;
 
-  char volumeSetIdentifier[128];
-  char publisherIdentifier[128];
-  char dataPreparerIdentifier[128];
-  char applicationIdentifier[128];
-  char copyrightFileIdentifier[38];
-  char abstractFileIdentifier[36];
-  char bibliographicFileIdentifier[37];
+        char volumeSetIdentifier[128];
+        char publisherIdentifier[128];
+        char dataPreparerIdentifier[128];
+        char applicationIdentifier[128];
+        char copyrightFileIdentifier[38];
+        char abstractFileIdentifier[36];
+        char bibliographicFileIdentifier[37];
 
-  Date volumeCreationDateTime;
-  Date volumeModificationDateTime;
-  Date volumeExpirationDateTime;
-  Date volumeEffectiveDateTime;
+        Date volumeCreationDateTime;
+        Date volumeModificationDateTime;
+        Date volumeExpirationDateTime;
+        Date volumeEffectiveDateTime;
 
-  int8_t fileStructureVersion;
-  int8_t unused4;
+        int8_t fileStructureVersion;
+        int8_t unused4;
   
-  uint8_t applicationUsed[512];
-  uint8_t isoReserved[653];
+        uint8_t applicationUsed[512];
+        uint8_t isoReserved[653];
 } __packed PrimaryVolumeDescriptor;
 
 /**
  * ISO9660 Disk Data.
  */
 typedef struct {
-  // Root sector read from the filesystem.
-  Sector rootSector;
+        /* Root sector read from the filesystem */
+        Sector rootSector;
 
-  // Sector to operate temporary data.
-  Sector tempSector;  
+        // Sector to operate temporary data.
+        Sector tempSector;  
 } FilesystemData;
 
 /**
  * Cursor for reading sector by sector
  */
 typedef struct {
-  Sector sector;
-  uint32_t lba;
-  uint8_t* cursor;
+        Sector sector;
+        uint32_t lba;
+        uint8_t* cursor;
 } FilesystemEntryCursor;
 
 /**
  * Entry in the file table.
  */
 typedef struct {
-  uint32_t filenameHash;
-  uint32_t lba;
-  uint32_t size;
+        uint32_t filenameHash;
+        uint32_t lba;
+        uint32_t size;
 } FilesystemEntry;
 
 /**
  * Filesystem Header Table.
  */
 typedef struct {
-  uint32_t numEntries;
+        uint32_t numEntries;
 
-  // Entries will point to an user allocated memory region that will
-  // store all entries. When deallocating this, user should free the
-  // 'entries' pointer.
-  FilesystemEntry *entries;
+        // Entries will point to an user allocated memory region that will
+        // store all entries. When deallocating this, user should free the
+        // 'entries' pointer.
+        FilesystemEntry *entries;
 } FilesystemHeaderTable;
 
 /**
@@ -206,7 +206,7 @@ extern int readFilesystem(FilesystemData *fsData);
  *                 recordFunction.
  */
 extern void navigateFilesystem(FilesystemData *fsData, 
-  RecordFunction recordFunction, void *userData);
+    RecordFunction recordFunction, void *userData);
 
 /**
  * Print (dbgio_buffer) every entry found in the filesystem.
@@ -226,7 +226,7 @@ extern uint32_t getHeaderTableSize(FilesystemData *fsData);
  * getHeaderTableSize() bytes available.
  */
 extern void fillHeaderTable(FilesystemData *fsData, 
-  FilesystemHeaderTable *headerTable);
+    FilesystemHeaderTable *headerTable);
 
 /**
  * Return file entry.
@@ -237,7 +237,7 @@ extern void fillHeaderTable(FilesystemData *fsData,
  *                       the file entry on the header table.
  */
 extern void getFileEntry(FilesystemHeaderTable *headerTable, 
-  uint32_t filenameHash, FilesystemEntry **resultingEntry);
+    uint32_t filenameHash, FilesystemEntry **resultingEntry);
 
 /**
  * Return file contents from the specified entry.
@@ -283,4 +283,4 @@ extern uint32_t generateHash(const char* filename, uint32_t length, uint32_t sta
  */
 extern uint32_t getFilenameHash(const char *filename, uint32_t length);
 
-
+#endif /* !_BLOCK_H_ */
--- cdblock/main.c
+++ cdblock/main.c
@@ -13,143 +13,141 @@
 #include <stdlib.h>
 
 #include "block.h"
-//#include "filesystem.h"
 
 static FilesystemData sCdFilesystemData;
 static FilesystemHeaderTable sCdHeaderTable;
 
-// wait for A button press.
-void getinput()
+/* Wait for A button press. */
+void
+getinput()
 {
-    static smpc_peripheral_digital_t _digital;
-    bool held = false;
-
-    smpc_peripheral_process();
-    smpc_peripheral_digital_port(1, &_digital);
-
-    held = ((_digital.pressed.raw & PERIPHERAL_DIGITAL_A) != 0x0000);
-
-    while(true)
-    {
-        dbgio_flush();
-        vdp_sync();
-
-        if(((_digital.pressed.raw & PERIPHERAL_DIGITAL_A) != 0x0000) &&
-           held == false)
-        {
-            break;
-        }
+        static smpc_peripheral_digital_t _digital;
+        bool held = false;
 
         smpc_peripheral_process();
         smpc_peripheral_digital_port(1, &_digital);
 
-        if(held)
-        {
-            held = ((_digital.pressed.raw & PERIPHERAL_DIGITAL_A) != 0x0000);
+        held = ((_digital.pressed.raw & PERIPHERAL_DIGITAL_A) != 0x0000);
+
+        while(true) {
+                dbgio_flush();
+                vdp_sync();
+
+                if(((_digital.pressed.raw & PERIPHERAL_DIGITAL_A) != 0x0000) && !held) {
+                        break;
+                }
+
+                smpc_peripheral_process();
+                smpc_peripheral_digital_port(1, &_digital);
+
+                if(held) {
+                        held = ((_digital.pressed.raw & PERIPHERAL_DIGITAL_A) != 0x0000);
+                }
         }
-    }
 }
 
-void clearscreen()
+static void
+clearscreen()
 {
-    dbgio_puts("[H[2J");
+        dbgio_puts("[H[2J");
 }
 
 static void _vblank_out_handler(void *);
 
-void initializeFilesystem() {
-  // CDBlock Initialization.
-  const int stat = initializeCDBlock();
-  assert(stat == 0);
-
-  readFilesystem(&sCdFilesystemData);
-
-  // Create cd entries table (necessary for looking for files).
-  const uint32_t tableSize = getHeaderTableSize(&sCdFilesystemData);
-
-  sCdHeaderTable.entries = (FilesystemEntry*) malloc(tableSize);
-
-  fillHeaderTable(&sCdFilesystemData, &sCdHeaderTable);
-}
-
-void hardwareInit() {
-  vdp2_tvmd_display_res_set(VDP2_TVMD_INTERLACE_NONE, VDP2_TVMD_HORZ_NORMAL_A,
-      VDP2_TVMD_VERT_224);
-
-  vdp2_scrn_back_screen_color_set(VDP2_VRAM_ADDR(3, 0x01FFFE),
-      COLOR_RGB1555(1, 0, 3, 15));
-
-  cpu_intc_mask_set(0);
-
-  vdp2_tvmd_display_set();
-  
-  vdp_sync_vblank_out_set(_vblank_out_handler);
-}
-
-void printFileContents(const char* filename)
+void
+initializeFilesystem()
 {
-  char readbuffer[250];
-  FilesystemEntry* fsEntry = NULL;
-  FilesystemEntryCursor fileCursor;
-  getFileEntry(&sCdHeaderTable, getFilenameHash(filename, strlen(filename)), &fsEntry);
+        const int stat = initializeCDBlock();
+        assert(stat == 0);
 
-  assert(fsEntry != NULL);
+        readFilesystem(&sCdFilesystemData);
 
-  initFileEntryCursor(fsEntry, &fileCursor);
-  int readAmount = sizeof(readbuffer);
-  int remainder = fsEntry->size;
+        /* Create cd entries table (necessary for looking for files) */
+        const uint32_t tableSize = getHeaderTableSize(&sCdFilesystemData);
 
-  do
-  {
-    if(readAmount > remainder)
-    {
-      readAmount = remainder;
-    }
+        sCdHeaderTable.entries = (FilesystemEntry*) malloc(tableSize);
 
-    int retCode = readFileCursor(fsEntry, &fileCursor, readbuffer, readAmount);
-    assert(retCode == 0);
-
-    remainder -= readAmount;
-
-    clearscreen();
-    dbgio_printf("%.*s", readAmount, readbuffer);
-    dbgio_printf("\nBytes read: %d\nBytes remaining: %d\nPress A to continue...", readAmount, remainder);
-    getinput();
-
-  } while (remainder > 0);
+        fillHeaderTable(&sCdFilesystemData, &sCdHeaderTable);
 }
 
-int main(void) {
-  hardwareInit();
+static void
+hardwareInit()
+{
+        vdp2_tvmd_display_res_set(VDP2_TVMD_INTERLACE_NONE, VDP2_TVMD_HORZ_NORMAL_A,
+            VDP2_TVMD_VERT_224);
 
-  dbgio_dev_default_init(DBGIO_DEV_VDP2_ASYNC);
-  dbgio_dev_font_load();
-  dbgio_dev_font_load_wait();
+        vdp2_scrn_back_screen_color_set(VDP2_VRAM_ADDR(3, 0x01FFFE),
+            COLOR_RGB1555(1, 0, 3, 15));
 
-  // Start filesystem
-  initializeFilesystem();
+        cpu_intc_mask_set(0);
+
+        vdp2_tvmd_display_set();
   
-  // test a file in a directory
-  printFileContents("A_FOLDER/DIRTEST.TXT");
-
-  // test a really big file
-  printFileContents("SHERLOCK.TXT");
-  
-  clearscreen();
-  dbgio_printf("All done!");
-
-  dbgio_flush();
-  vdp_sync();
-
-  while (true)
-    ;
+        vdp_sync_vblank_out_set(_vblank_out_handler);
 }
 
+void
+printFileContents(const char* filename)
+{
+        char readbuffer[250];
+        FilesystemEntry* fsEntry = NULL;
+        FilesystemEntryCursor fileCursor;
+        getFileEntry(&sCdHeaderTable, getFilenameHash(filename, strlen(filename)), &fsEntry);
 
+        assert(fsEntry != NULL);
+
+        initFileEntryCursor(fsEntry, &fileCursor);
+        int readAmount = sizeof(readbuffer);
+        int remainder = fsEntry->size;
+
+        do {
+                if(readAmount > remainder) {
+                        readAmount = remainder;
+                }
+
+                int retCode = readFileCursor(fsEntry, &fileCursor, readbuffer, readAmount);
+                assert(retCode == 0);
+
+                remainder -= readAmount;
+
+                clearscreen();
+                dbgio_printf("%.*s", readAmount, readbuffer);
+                dbgio_printf("\nBytes read: %d\nBytes remaining: %d\nPress A to continue...", readAmount, remainder);
+                getinput();
+
+        } while (remainder > 0);
+}
+
+void
+main(void)
+{
+        hardwareInit();
+
+        dbgio_dev_default_init(DBGIO_DEV_VDP2_ASYNC);
+        dbgio_dev_font_load();
+        dbgio_dev_font_load_wait();
+
+        /* Start filesystem */
+        initializeFilesystem();
+  
+        /* Test a file in a directory */
+        printFileContents("A_FOLDER/DIRTEST.TXT");
+
+        /* Test a really big file */
+        printFileContents("SHERLOCK.TXT");
+  
+        clearscreen();
+        dbgio_printf("All done!");
+
+        dbgio_flush();
+        vdp_sync();
+
+        while (true) {
+        }
+}
 
 static void
 _vblank_out_handler(void *work __unused)
 {
-  smpc_peripheral_intback_issue();
+        smpc_peripheral_intback_issue();
 }
-
