fb8e9a98464aae3fae82eb77ccc34ddd853ee175
hitomi2500 <hitomi2500@mail.ru>
2021-09-01 00:30:13 +0300

    Normal (non-DMAC) SCI read and write test (unfinished)

--- cpu-sci/cpu-sci.c
+++ cpu-sci/cpu-sci.c
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 //some test locations for read and write buffers
-#define SCI_BUFFER 0x26080000
+#define SCI_BUFFER_SEND 0x26080000
 #define SCI_BUFFER_RECV 0x26090000
 
 #define TEST_PATTERN_LENGTH 0x10000 
@@ -21,7 +21,8 @@
 static void _dmac_handler(void *);
 static void __unused _sci_handler(void *);
 
-static volatile bool _done = false;
+static volatile bool _dma_done = false;
+static volatile bool _sci_done = false;
 
 int
 main(void)
@@ -32,12 +33,14 @@ main(void)
         dbgio_dev_font_load();
         dbgio_dev_font_load_wait();
 
+        //For a first part, let's setup SCI in a "tandem" mode, where it works together with DMAC
+
         cpu_dmac_interrupt_priority_set(8);
         cpu_sci_interrupt_priority_set(0); //disable interrupts from SCI, because they will be served by DMAC, not CPU
  
         cpu_dmac_cfg_t cfg0 __unused = {
                 .channel= 0,
-                .src = SCI_BUFFER,
+                .src = SCI_BUFFER_SEND,
                 .src_mode = CPU_DMAC_SOURCE_INCREMENT,
                 .dst = CPU(TDR),
                 .dst_mode = CPU_DMAC_DESTINATION_FIXED,
@@ -85,7 +88,7 @@ main(void)
         //prepare write buffer
         for (i=0;i<TEST_PATTERN_LENGTH;i++)
         {
-                MEMORY_WRITE(8,SCI_BUFFER+i,TEST_PATTERN_START+i);
+                MEMORY_WRITE(8,SCI_BUFFER_SEND+i,TEST_PATTERN_START+i);
         }
 
         //clear read buffer
@@ -101,7 +104,7 @@ main(void)
         cpu_dmac_enable();
         
         //start SCI with DMAC, it requires disabling and enabling it in a dmac mode
-        _done = false;
+        _dma_done = false;
         cpu_sci_reset_status();
         cpu_sci_disable();
         cpu_dmac_channel_start(1); //start the read channel first, so it's with sync with write channel
@@ -109,7 +112,7 @@ main(void)
         cpu_sci_enable_with_dmac(&cfg_sci); //enable SCI back, the requests to DMAC should start automatically
         
         //wait for DMA
-        while (!_done) ;
+        while (!_dma_done) ;
 
         //datacheck
         int iErrors = 0;
@@ -131,7 +134,75 @@ main(void)
                 }
         }
 
-        dbgio_printf("SCI loopback test error count = %u\n",iErrors);
+        dbgio_printf("SCI-DMAC loopback error count = %u\n",iErrors);
+
+        if (iErrors)
+        {
+                dbgio_printf("\nSCI-DMAC loopback test failed.\nEither pins 5 and 6 on CN6 (serial port)\nare not shorted, or you're running \nthe test within an emulator.\n");
+        }
+        else
+        {
+                dbgio_printf("\nSCI-DMAC loopback test was sucessful.\n");
+        }
+
+        dbgio_flush();
+        vdp_sync(); 
+
+        //For a second part, let's setup SCI in a normal mode, with CPU interrupt and without DMAC link
+
+        cpu_dmac_disable();
+        cpu_sci_disable();
+
+        //disabling RXI and TXI interrupts.
+        cfg_sci.ihr_rxi = NULL;
+        cfg_sci.ihr_txi = NULL;
+
+        //enabling TEI (end of transfer) interrupt
+        cfg_sci.ihr_tei = _sci_handler;
+
+        //re-setup the sci 
+        cpu_sci_config_set(&cfg_sci);
+
+        //reset any prior interrupts
+        cpu_sci_reset_status();
+
+        //enable interrupts from SCI
+        cpu_sci_interrupt_priority_set(8); 
+
+        //clear read buffer
+        memset((uint8_t*)SCI_BUFFER_RECV,0x00,TEST_PATTERN_LENGTH);
+
+        //do a bunch of transfers
+        for (i=0;i<TEST_PATTERN_LENGTH;i++)
+        {
+                _sci_done = false;
+                cpu_sci_set_write_value(TEST_PATTERN_START+i);
+                cpu_sci_enable();
+                while (false == _sci_done)
+                        ;
+                MEMORY_WRITE(8,SCI_BUFFER_RECV+i,cpu_sci_get_read_value());
+        }
+
+        //check
+        iErrors = 0;
+        for (i=0;i<TEST_PATTERN_LENGTH;i++)
+        {
+                readen = MEMORY_READ(8,SCI_BUFFER_RECV+i);
+                written = TEST_PATTERN_START+i;
+
+                if (readen !=written)
+                {
+                        if (iErrors < 10)
+                        {
+                                dbgio_printf("ERR: pos %u wr = 0x%02lX rd = 0x%02lX\n",iErrors,written,readen);
+                                dbgio_flush();
+                                vdp_sync();
+                        }
+                        iErrors ++; 
+                }
+        }
+
+        dbgio_printf("SCI loopback error count = %u\n",iErrors);
 
         if (iErrors)
         {
@@ -143,7 +214,7 @@ main(void)
         }
 
         dbgio_flush();
-        vdp_sync(); 
+        vdp_sync();
 
         while (true) {
         }
@@ -168,11 +239,12 @@ user_init(void)
 static void
 _dmac_handler(void *work __unused)
 {
-        _done = true;
+        _dma_done = true;
 }
 
 static void __unused
 _sci_handler(void *work __unused)
 {
+        _sci_done = true;
 }
 
