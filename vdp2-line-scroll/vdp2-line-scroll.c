1f411bd6c457f222c502e2551a0a893aec06e942
Israel Jacquez <mrkotfw@gmail.com>
2022-03-13 14:22:15 -0700

    Expose actual values in line scroll value

--- vdp2-line-scroll/vdp2-line-scroll.c
+++ vdp2-line-scroll/vdp2-line-scroll.c
@@ -4,15 +4,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define NBG0_CPD                VDP2_VRAM_ADDR(0, 0x00000)
-#define NBG0_PND                VDP2_VRAM_ADDR(2, 0x00000)
-#define NBG0_PAL                VDP2_CRAM_MODE_1_OFFSET(0, 0, 0)
-#define NBG0_LINE_SCROLL        VDP2_VRAM_ADDR(1, 0x00000)
+#define NBG0_CPD         VDP2_VRAM_ADDR(0, 0x00000)
+#define NBG0_PND         VDP2_VRAM_ADDR(0, 0x02000)
+#define NBG0_PAL         VDP2_CRAM_MODE_1_OFFSET(0, 0, 0)
+#define NBG0_LINE_SCROLL VDP2_VRAM_ADDR(0, 0x10000)
 
-#define BACK_SCREEN             VDP2_VRAM_ADDR(3, 0x1FFFE)
+#define BACK_SCREEN      VDP2_VRAM_ADDR(3, 0x1FFFE)
 
-extern uint8_t asset_line_scroll_tbl[];
-extern uint8_t asset_line_scroll_tbl_end[];
 extern uint8_t asset_vf_cpd[];
 extern uint8_t asset_vf_cpd_end[];
 extern uint8_t asset_vf_pnd[];
@@ -20,38 +18,37 @@ extern uint8_t asset_vf_pnd_end[];
 extern uint8_t asset_vf_pal[];
 extern uint8_t asset_vf_pal_end[];
 
-static vdp2_scrn_ls_format_t _ls_format = {
-        .scroll_screen     = VDP2_SCRN_NBG0,
-        .line_scroll_table = NBG0_LINE_SCROLL,
-        .interval          = 0,
-        .enable            = VDP2_SCRN_LS_HORZ
-};
-
 void
 main(void)
 {
-        scu_dma_transfer(0, (void *)NBG0_CPD, asset_vf_cpd, asset_vf_cpd_end - asset_vf_cpd);
-        scu_dma_transfer(0, (void *)NBG0_PND, asset_vf_pnd, asset_vf_pnd_end - asset_vf_pnd);
-        scu_dma_transfer(0, (void *)NBG0_PAL, asset_vf_pal, asset_vf_pal_end - asset_vf_pal);
+        vdp2_scrn_ls_format_t ls_format = {
+                .scroll_screen = VDP2_SCRN_NBG0,
+                .table         = NBG0_LINE_SCROLL,
+                .interval      = 0,
+                .enable        = VDP2_SCRN_LS_HORZ
+        };
 
-        scu_dma_transfer(0, (void *)NBG0_LINE_SCROLL, asset_line_scroll_tbl, asset_line_scroll_tbl_end - asset_line_scroll_tbl);
+        volatile vdp2_scrn_ls_h_t *_line_scroll_tbl =
+            (volatile vdp2_scrn_ls_h_t *)NBG0_LINE_SCROLL;
 
-        vdp2_scrn_ls_set(&_ls_format);
+        fix16_t speed;
+        speed = FIX16(0.0f);
 
-        vdp2_sync();
-        vdp2_sync_wait();
-
-        uint16_t i;
-        i = 0;
+        const fix16_t amplitude = FIX16(120.0f);
 
         while (true) {
-                _ls_format.line_scroll_table = NBG0_LINE_SCROLL + (i << 2);
+                for (uint32_t i = 0; i < 240; i++) {
+                        const fix16_t value = fix16_mul(fix16_int32_from(i), FIX16(M_PI / 240.0f)) + speed;
 
-                vdp2_scrn_ls_set(&_ls_format);
+                        _line_scroll_tbl[i].horz = fix16_mul(amplitude, fix16_sin(value));
+                }
+
+                speed += FIX16(0.0125f);
+
+                vdp2_scrn_ls_set(&ls_format);
+
                 vdp2_sync();
                 vdp2_sync_wait();
-
-                i = (i + 1) & 0x00FF;
         }
 }
 
@@ -93,30 +90,22 @@ user_init(void)
         vdp2_sprite_priority_set(7, 0);
 
         const vdp2_vram_cycp_bank_t vram_cycp_bank_a0 = {
-                .t0 = VDP2_VRAM_CYCP_CHPNDR_NBG0,
-                .t1 = VDP2_VRAM_CYCP_CHPNDR_NBG0,
-                .t2 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t3 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t4 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t5 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t6 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t7 = VDP2_VRAM_CYCP_NO_ACCESS
+                .t0 = VDP2_VRAM_CYCP_PNDR_NBG0,
+                .t1 = VDP2_VRAM_CYCP_CPU_RW,
+                .t2 = VDP2_VRAM_CYCP_CPU_RW,
+                .t3 = VDP2_VRAM_CYCP_CPU_RW,
+                .t4 = VDP2_VRAM_CYCP_CHPNDR_NBG0,
+                .t5 = VDP2_VRAM_CYCP_CHPNDR_NBG0,
+                .t6 = VDP2_VRAM_CYCP_CPU_RW,
+                .t7 = VDP2_VRAM_CYCP_CPU_RW
         };
 
         vdp2_vram_cycp_bank_set(0, &vram_cycp_bank_a0);
 
-        const vdp2_vram_cycp_bank_t vram_cycp_bank_b0 = {
-                .t0 = VDP2_VRAM_CYCP_PNDR_NBG0,
-                .t1 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t2 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t3 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t4 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t5 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t6 = VDP2_VRAM_CYCP_NO_ACCESS,
-                .t7 = VDP2_VRAM_CYCP_NO_ACCESS
-        };
-
-        vdp2_vram_cycp_bank_set(2, &vram_cycp_bank_b0);
+        scu_dma_transfer(0, (void *)NBG0_CPD, asset_vf_cpd, asset_vf_cpd_end - asset_vf_cpd);
+        scu_dma_transfer(0, (void *)NBG0_PND, asset_vf_pnd, asset_vf_pnd_end - asset_vf_pnd);
+        scu_dma_transfer(0, (void *)NBG0_PAL, asset_vf_pal, asset_vf_pal_end - asset_vf_pal);
+        scu_dma_transfer_wait(0);
 
         vdp2_tvmd_display_set();
 }
