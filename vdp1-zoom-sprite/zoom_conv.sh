cf43d51e279adb1bbde41aa3552779254a9c18ef
Israel Jacquez <mrkotfw@gmail.com>
2022-06-30 00:25:35 -0700

    Update vdp1-zoom-sprite/zoom_conv.sh

--- vdp1-zoom-sprite/zoom_conv.sh
+++ vdp1-zoom-sprite/zoom_conv.sh
@@ -20,6 +20,6 @@ cd ..
 # Convert
 grit "data/zoom_appended.png" -g -gb -gB8 -m\! -p -pT1 -fh\! -ftb
 
-# Move to ROMDISK
-mv "zoom_appended.pal.bin" "romdisk/ZOOM.PAL"
-mv "zoom_appended.img.bin" "romdisk/ZOOM.TEX"
+# Move to assets/
+mv "zoom_appended.pal.bin" "assets/ZOOM.PAL"
+mv "zoom_appended.img.bin" "assets/ZOOM.TEX"
