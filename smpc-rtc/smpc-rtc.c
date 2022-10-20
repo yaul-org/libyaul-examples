811953f7eac539f0101bf0b2c1d27d631c5d5eb9
Israel Jacquez <mrkotfw@gmail.com>
2022-10-19 20:04:25 -0700

    Update smpc-rtc example

--- smpc-rtc/smpc-rtc.c
+++ smpc-rtc/smpc-rtc.c
@@ -8,6 +8,7 @@
 #include <yaul.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define BACK_SCREEN VDP2_VRAM_ADDR(3, 0x1FFFE)
@@ -61,14 +62,30 @@ main(void)
         dbgio_dev_font_load();
 
         const smpc_time_t * const time = smpc_rtc_time_get();
+        smpc_time_dec_t time_dec;
+
+        /* Wait until the SMPC peripheral INTBACK command has been issued at
+         * least once */
+        vdp2_tvmd_vblank_in_wait();
+        vdp2_tvmd_vblank_out_wait();
+        smpc_peripheral_process();
+        smpc_rtc_time_bcd_from(time, &time_dec);
+        /* Use SMPC RTC date as a 32-bit seed for the default PRNG */
+        const uint32_t seed = ((uint32_t)time_dec.month * 2629800UL) +
+                              ((uint32_t)time_dec.day * 86400UL) +
+                              ((uint32_t)time_dec.hours * 3600UL) +
+                              ((uint32_t)time_dec.minutes * 60UL) +
+                               (uint32_t)time_dec.seconds;
+
+        dbgio_printf("[HUsing seed: %lu\n", seed);
+
+        srand(seed);
 
         while (true) {
                 smpc_peripheral_process();
 
-                dbgio_printf("[H[2J");
+                dbgio_printf("[3;1H[1J");
 
-                smpc_time_dec_t time_dec;
-                smpc_rtc_time_bcd_from(time, &time_dec);
 
                 dbgio_printf("%i/%i/%i %02i:%02i:%02i\n",
                     time_dec.day,
@@ -85,6 +102,8 @@ main(void)
                     _number_suffix_string[time->day & 0xF],
                     _month_strings[time_dec.month],
                     time_dec.year);
+
+                dbgio_printf("Random value, %lu\n", (rand() & 15));
 
                 dbgio_flush();
                 vdp2_sync();
