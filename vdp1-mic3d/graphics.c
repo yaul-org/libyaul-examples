9ae620c197ca7d8923a64c0f7bdb631991ff3025
Israel Jacquez <mrkotfw@gmail.com>
2022-12-31 15:42:12 -0800

    Add picture to list

--- vdp1-mic3d/graphics.c
+++ vdp1-mic3d/graphics.c
@@ -1,7 +1,9 @@
 #include "mic3d.h"
 
 extern const picture_t picture_mika;
+extern const picture_t picture_tails;
 
 const picture_t *pictures[] = {
-        &picture_mika
+        &picture_mika,
+        &picture_tails
 };
